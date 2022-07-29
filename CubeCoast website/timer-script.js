3056b0c532130b719654dae2bf6924dbebdfb36d
Paardhiv Sarakam <88365669+Paardhiv10@users.noreply.github.com>
2022-07-29 15:17:25 +0530

    Add files via upload

---
+++ CubeCoast website/timer-script.js
@@ -0,0 +1,19 @@
+var x= document.getElementById('scramble');
+const scramble3 = ["U", "R", "D", "L", "F", "B", "U'", "R'", "D'", "L'", "F'", "B'", "U2", "R2", "D2", "L2", "F2", "B2"];
+//let y = scramble3.join();
+
+function getRndInteger(min, max) {
+    var resulttext = "";
+    for(var i=0;i<max;i++){
+        resulttext = resulttext +' '+ scramble3[Math.floor(Math.random()*18)];
+    }
+    x.innerHTML = resulttext;
+    x.style.color = 'white';
+    x.style.textAlign = 'center';
+    x.style.fontSize = '40px';
+
+}
+
+
+
+
